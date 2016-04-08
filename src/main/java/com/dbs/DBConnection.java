fad47d33b6570c19baaa18b717de67191de87186
Anghel Contiu <anghel.contiu@endava.com>
2016-04-08 14:53:54 +0300

    first commit - experiment spring boot + H2 in memory DB

---
+++ src/main/java/com/dbs/DBConnection.java
@@ -0,0 +1,79 @@
+package com.dbs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+/**
+ * Created by anghelc on 08/04/16.
+ */
+public class DBConnection {
+
+    private static final Logger LOGGER  = LoggerFactory.getLogger(DBConnection.class);
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/test";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection connectToDB() {
+        LOGGER.info("connecting to DB");
+
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+                    DB_PASSWORD);
+            dbConnection.setAutoCommit(false);
+            LOGGER.info("Created DB Connection.");
+            insertWithPreparedStatement(dbConnection);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
+
+    private static void insertWithPreparedStatement(Connection connection) throws SQLException {
+        PreparedStatement createPreparedStatement = null;
+        PreparedStatement insertPreparedStatement = null;
+        PreparedStatement selectPreparedStatement = null;
+
+        String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
+        String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
+        String SelectQuery = "select * from PERSON";
+        try {
+            connection.setAutoCommit(false);
+
+            createPreparedStatement = connection.prepareStatement(CreateQuery);
+            createPreparedStatement.executeUpdate();
+            createPreparedStatement.close();
+
+            insertPreparedStatement = connection.prepareStatement(InsertQuery);
+            insertPreparedStatement.setInt(1, 1);
+            insertPreparedStatement.setString(2, "Jose");
+            insertPreparedStatement.executeUpdate();
+            insertPreparedStatement.close();
+
+            selectPreparedStatement = connection.prepareStatement(SelectQuery);
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            System.out.println("H2 Database inserted through PreparedStatement");
+            while (rs.next()) {
+                System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name"));
+            }
+            selectPreparedStatement.close();
+
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+}
